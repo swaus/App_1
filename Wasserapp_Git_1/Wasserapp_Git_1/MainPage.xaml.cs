9ddb5aee6e49a984a13de3263838b8de9f4cc70b
Sarah Wauschkuhn <s0564506@htw-berlin.de>
2018-10-20 14:56:01 +0200

    Projektdateien hinzufügen.

---
+++ Wasserapp_Git_1/Wasserapp_Git_1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Wasserapp_Git_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
